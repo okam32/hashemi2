21ac1603a3f65f34b7a72e6243a64f1b210ba366
okam32 <mohamad.okam70@gmail.com>
2024-08-30 19:13:30 +0330

    DTOS

---
+++ hashemi2/Core/Dtos/RegisterDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace hashemi2.Core.Dtos
+{
+    public class RegisterDto
+    {
+        [Required(ErrorMessage = "UserName is required")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+    }
+}
---
+++ hashemi2/Core/Dtos/YpdatePermisionDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace hashemi2.Core.Dtos
+{
+    public class YpdatePermisionDto
+    {
+        [Required(ErrorMessage = "UserName is required")]
+        public string UserName { get; set; }
+    }
+}
