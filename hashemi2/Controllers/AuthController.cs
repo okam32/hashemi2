9cd0c4a1e1255d42defe7213d3cb7ba33eba6aa0
okam32 <mohamad.okam70@gmail.com>
2024-09-05 14:31:38 +0330

    v3

---
+++ hashemi2/Controllers/AuthController.cs
@@ -0,0 +1,130 @@
+﻿using hashemi2.Core.Dtos;
+using hashemi2.Core.OtherObjects;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace hashemi2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+        }
+
+
+        //seeding my roles to db
+        [HttpPost]
+        [Route("seed-roles")]
+        public async Task<IActionResult> SeedRoles()
+        {
+            bool isOwnerExist = await _roleManager.RoleExistsAsync(StaticUserRole.OWNER);
+            bool isAdminExist = await _roleManager.RoleExistsAsync(StaticUserRole.ADMIN);
+            bool isUserExist = await _roleManager.RoleExistsAsync(StaticUserRole.USER);
+
+            if (isOwnerExist && isAdminExist && isUserExist)
+                return Ok("User role alrdy exists");
+
+
+            await _roleManager.CreateAsync(new IdentityRole(StaticUserRole.USER));
+            await _roleManager.CreateAsync(new IdentityRole(StaticUserRole.ADMIN));
+            await _roleManager.CreateAsync(new IdentityRole(StaticUserRole.OWNER));
+
+            return Ok("Seed role done sucessfuly");
+        }
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
+        {
+            var isExistsUser = await _userManager.FindByNameAsync(registerDto.UserName);
+
+            if (isExistsUser != null)
+            {
+                return BadRequest("username alrdy exists");
+            }
+
+            IdentityUser newUser = new IdentityUser()
+            {
+                Email = registerDto.Email,
+                UserName = registerDto.UserName,
+                SecurityStamp = Guid.NewGuid().ToString(),
+            };
+
+            var createUserResult =  await _userManager.CreateAsync(newUser,registerDto.Password);
+
+            if (!createUserResult.Succeeded)
+            {
+                var errorString = "user creation failed beacause: ";
+                foreach (var error in createUserResult.Errors)
+                {
+                    errorString += " # " + error.Description;
+                }
+                return BadRequest(errorString);
+            }
+            await _userManager.AddToRoleAsync(newUser,StaticUserRole.USER);
+
+            return Ok("user created successfully");
+        }
+
+        //roue -> login
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            var user = await _userManager.FindByNameAsync(loginDto.UserName);
+            if (user is null)
+                return Unauthorized("invalid credentials");
+
+            var isPasswordCorrect = await _userManager.CheckPasswordAsync(user, loginDto.Password);
+            if(!isPasswordCorrect)
+                return Unauthorized("invalid credentials");
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            var authClaims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name,user.UserName),
+                new Claim(ClaimTypes.NameIdentifier,user.Id),
+                new Claim("JWTID",Guid.NewGuid().ToString()),
+            };
+
+            foreach (var userRole in userRoles)
+            {
+                authClaims.Add(new Claim(ClaimTypes.Role,userRole));
+            }
+
+            var token = GenerateNewJasonWebToken(authClaims);
+            return Ok(token);
+
+            
+        }
+        private string GenerateNewJasonWebToken(List<Claim> claims)
+        {
+            var authSecret = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+
+            var tokenObject = new JwtSecurityToken(
+                    issuer:_configuration["JWT:ValidIssuer"],
+                    audience: _configuration["JWT:ValidAudience"],
+                    expires: DateTime.Now.AddHours(1),
+                    claims: claims,
+                    signingCredentials: new SigningCredentials(authSecret,SecurityAlgorithms.HmacSha256)
+                );
+            string token = new JwtSecurityTokenHandler().WriteToken(tokenObject);
+            return token;
+        }
+    }
+}
+
